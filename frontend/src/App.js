81a3894e3cbab0a1b8dcfe7d5e52031cbfc50056
jasmine05102005 <jashu05102005@gmail.com>
2025-09-28 18:08:57 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chatbot from "./components/Chatbot";
+
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center", marginTop: 20 }}>Vehicle Service Chatbot</h2>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
